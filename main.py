b9c91f26f64e713399ab0465622c3d9cd8625040
Jake R <73565590+jakerothstein@users.noreply.github.com>
2024-06-23 07:02:57 -0700

    Control of playlist name
    This allows you to update the name of the playlist that @IlyaSlesar added (I made some changes) and I also added some optimization and also an success output that tests to see if the playlist was created and if the songs properly filled
    
    Co-Authored-By: IlyaSlesar <52408612+ilyaslesar@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ CLIENT_ID = ""
 CLIENT_SECRET = ""
 REDIRECT_URI = "http://localhost:8888/callback"  # Set this to "http://localhost:8888/callback" to run it locally
 USERNAME = ""  # Put the username of the account registered with the API token
+PLAYLIST_NAME = "Liked Songs Export"
 
 scope = 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-library-read user-top-read'
 
@@ -19,16 +20,26 @@ token = util.prompt_for_user_token(
 
 sp = spotipy.Spotify(auth=token)
 
-sp.user_playlist_create(user=USERNAME, name="Liked Songs", public=False)
-playlist_id = sp.user_playlists(user=USERNAME)["items"][0]["id"]
+out = sp.user_playlist_create(user=USERNAME, name=PLAYLIST_NAME, public=False)
+playlist_id = out["id"]
+playlists = sp.user_playlists(user=USERNAME)["items"]
 
 song_list = []
 tracks = sp.current_user_saved_tracks(limit=50, offset=0)
 for i in range(math.ceil((int(tracks['total'])) / 50)):
-
     for item in tracks["items"]:
         song_list.append(item["track"]["external_urls"]["spotify"])
     sp.playlist_add_items(playlist_id=playlist_id, items=song_list)
     song_list = []
 
     tracks = sp.current_user_saved_tracks(limit=50, offset=((i + 1) * 50))
+
+lists = sp.user_playlists(user=USERNAME)["items"]
+for playlist in lists:
+    if playlist["id"] == playlist_id:
+        if playlist["tracks"]["total"] == tracks["total"]:
+            print(f"Playlist '{PLAYLIST_NAME}' created with {playlist['tracks']['total']} songs.")
+            break
+        else:
+            print("Error: Playlist created but songs not added. Try again and closing spotify while running the script.")
+            break
